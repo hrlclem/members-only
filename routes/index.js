ae87efc1ac2729dc4f39c4bbea3f88ebad69a95f
Clement <perso@clementhurel.fr>
2022-10-31 21:39:53 +0900

    improve design step 1

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-    title: 'Homepage (See all comments)',
+    title: 'The Comment Club',
     user: res.locals.currentUser,
   });
 });
